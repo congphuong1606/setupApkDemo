befa1ea96681706f28ca88f0fe78ce61d5a8b7e2
congphuong1606 <cphuong.coldboy@gmail.com>
2017-11-30 21:25:48 +0700

    dsff

---
+++ app/src/main/java/com/congp/app/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,48 @@
+package com.congp.app.fcm;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.congp.app.R;
+import com.congp.app.activity.main.MainActivity;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by congp on 11/30/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FCM Service";
+    Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher_background) // notification icon
+                .setContentTitle(remoteMessage.getFrom()) // title for notification
+                .setContentText(remoteMessage.getData().toString())
+                .setSound(alarmSound)
+                .setVibrate(new long[] { 1000, 1000})// message for notification
+                .setAutoCancel(true); // clear notification after click
+        Intent intent = new Intent(this, MainActivity.class);
+        @SuppressLint("WrongConstant") PendingIntent pi = PendingIntent.getActivity(this,0,intent,Intent.FLAG_ACTIVITY_NEW_TASK);
+        mBuilder.setContentIntent(pi);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
+
+
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+}
